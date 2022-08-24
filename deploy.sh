fb0f53881743e2247fd63c180f6ef33ea6bee22a
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2022-08-24 23:44:51 +0800

    :rocket: add deploy script

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+yarn build-only
+
+# 进入生成的文件夹
+cd ./dist
+
+# 如果是发布到自定义域名
+echo 'share.lovejade.cn' > CNAME
+
+echo 'google.com, pub-8586652723015758, DIRECT, f08c47fec0942fa0' > ads.txt
+
+git init
+git add -A
+git commit -m 'deploy update 🎉'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:nicejade/markdown2png.git master:gh-pages
+cd -
