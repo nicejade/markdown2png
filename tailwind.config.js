b4f0768622a6d5531d88b58654f69d2394fb4027
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2022-09-04 19:19:12 +0800

    :heavy_plus_sign: inject @headlessui/vue @heroicons/vue

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,27 @@ module.exports = {
     },
     colors: {
       gray: '#E2E8F0'
-    }
+    },
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ['hover', 'focus'],
+    }
   },
   corePlugins: {
     outline: false,
   },
-  plugins: [],
+  plugins: [
+    require('@headlessui/tailwindcss')
+  ],
 }
