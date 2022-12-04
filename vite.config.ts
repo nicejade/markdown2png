e1728415eeae6672c90b1584633e3d7ef8eb6acd
杨琼璞 <jeffygisgreat@gmail.com>
2022-12-05 00:15:40 +0800

    :bug: fix Top-level await is not available.

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import wasm from 'vite-plugin-wasm'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), wasm()],
+  build: {
+    target:['edge90','chrome90','firefox90','safari15']
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
