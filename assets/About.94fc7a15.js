edb888304fb0e966eac6d466fc2fbc0f65f21edf
杨琼璞 <jeffygisgreat@gmail.com>
2022-12-05 01:46:33 +0800

    deploy update 🎉

--- assets/About.94fc7a15.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as o,a as s}from"./index.f8491529.js";const _={},c={class:"about"},a=s("h1",null,"This is an about page",-1),n=[a];function r(i,u){return t(),o("div",c,n)}const l=e(_,[["render",r]]);export{l as default};
