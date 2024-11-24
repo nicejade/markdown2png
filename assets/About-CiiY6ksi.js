c79063a8666e145540310bc970511a25e30f5710
杨琼璞 <jeffygisgreat@gmail.com>
2024-11-24 23:04:27 +0800

    deploy update 🎉

--- assets/About-CiiY6ksi.js
+++ assets/About-CiiY6ksi.js
@@ -1,5 +1,5 @@
-import{d as a,p as o,c as n,a as s,u as r,o as c}from"./index-COOP5uyC.js";const e=`
-[玉桃文飨轩🍑](https://share.lovejade.cn/?utm_source=about) ，简单好用的在线文本工具；支持将 Markdown、富文本等格式内容，快速转化为 png、pdf、html 等文件，并支持自定义设置、一键下载等功能。 基于 Vue3、Vite、Pinia、TailwindCSS、TypeScript 构建。
+import{d as t,p as a,c as o,a as s,u as c,o as n}from"./index-BBRPxv93.js";const d=`
+[玉桃文飨轩🍑](https://share.lovejade.cn/?utm_source=about) ，简单好用的在线文本工具；支持将 [Markdown](https://site.lovejade.cn/tags/Markdown)、Rich Text 等格式内容，快速转化为 png、pdf、html 等文件，并支持自定义设置、一键下载等功能。基于 [Vue3](https://site.lovejade.cn/post/5b1a221c0526c920d6dfaada) 、 [Vite](https://site.lovejade.cn/post/6010e1b10c71de1fb957b64e) 、 [Pinia](https://site.lovejade.cn/post/631813c3c0922e0e572bde81) 、 [TailwindCSS](https://site.lovejade.cn/post/5fd20cb4c06d6302c1907ec7) 、 [TypeScript](https://site.lovejade.cn/post/6278fdeaac00ce3f9b11a8ef) 构建。
 
 ## 诞生初衷
 
@@ -22,4 +22,4 @@ import{d as a,p as o,c as n,a as s,u as r,o as c}from"./index-COOP5uyC.js";const
 
 早在 [关于“悠然宜想亭”的由来，以及未来](https://forum.lovejade.cn/d/1) 一文提及，对于网站取名多是五个字；或是兴趣的原因，其后缀带有 [亭台楼阁、轩榭廊舫，殿庭苑斋](https://forum.lovejade.cn/d/9) 其中一个；其中大部分已被使用，只好使用最喜欢的这个「轩」字。那时，佳人仍在，与之相处，一日三见「桃🍑」；独“桃”难以成词，择「玉」字以配之；至于「文飨」（飨与“享”通假），寄托这款工具可作为「分享文字」之用。时隔年余，更多功能竟未完善，一如那逝去的爱恋，惜哉，如此而已。
 
-2023 年 11 月 27 日补录于〔深圳福田〕`,u={id:"about",class:"w-full max-w-xl my-4"},d=["innerHTML"],_=a({__name:"About",setup(l){console.log(e);let t=o(e,{breaks:!0});return(i,p)=>(c(),n("div",u,[s("div",{class:"markdown",innerHTML:r(t)},null,8,d)]))}});export{_ as default};
+2023 年 11 月 27 日补录于〔深圳福田〕`,p={id:"about",class:"my-4 w-full max-w-xl"},r=["innerHTML"],m=t({__name:"About",setup(i){let e=a(d,{breaks:!0});return(l,u)=>(n(),o("div",p,[s("div",{class:"markdown",innerHTML:c(e)},null,8,r)]))}});export{m as default};
