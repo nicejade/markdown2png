f271abbd155fe43e8293cae8789fe5a64904aee3
杨琼璞 <jeffygisgreat@gmail.com>
2026-04-20 23:47:57 +0800

    ⚰️ Remove three bg images

--- src/views/Digest.vue
+++ src/views/Digest.vue
@@ -91,9 +91,6 @@ const backgrounds = ref([
   '/images/bg0.png',
   '/images/bg1.png',
   '/images/bg2.png',
-  '/images/bg3.png',
-  '/images/bg4.png',
-  '/images/bg5.png',
 ])
 
 const canvasRef = ref<HTMLCanvasElement | null>(null)
